521834b778739413933b40823af2ddb6700e7c50
Antonio Rafael Cedillo Rodríguez <116749018+AntonioCedilloA01656823@users.noreply.github.com>
2023-03-07 19:10:31 -0600

    Agrege endpoint niveles y empezando calificacion y login
    Los ultimos dos los acabo

--- fraccionados/endpoints/urls.py
+++ fraccionados/endpoints/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index')
+    path('login',views.login,name='login'),
+    path('niveles',views.niveles,name='niveles'),
+    path('calificacion',views.calificacion,name='calificacion'),
 ]
--- fraccionados/endpoints/views.py
+++ fraccionados/endpoints/views.py
@@ -1,7 +1,53 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from json import loads,dumps
 
 # Create your views here.
 
-def index(response):
-    return("La pagina indice funciona, Bienvenido a los endpoints")
+def index(request):
+    return HttpResponse("La pagina indice funciona, Bienvenido a los endpoints")
 
+
+def login(request):
+    return HttpResponse("Login funciona")
+
+
+
+@csrf_exempt
+def niveles(request):
+    body = request.body.decode('UTF-8')
+    eljson = loads(body)
+    numeroLevel = body['num_nivel']
+
+    if (numeroLevel == 1):
+        tema = "conjuntos"
+        descripcion = "Consigue los elementos que te menciona el juego y llevalos a la olla para conseguir puntos"
+        resultado = {"tematica":tema,"descripcion":descripcion}
+        res_json = resultado.toJSON()
+        return HttpResponse(res_json,content_type ="text/json-comment-filtered")
+    
+    elif (numeroLevel == 2):
+        tema = "Operaciones"
+        descripcion = "Reune los elementos necesarios que te diga la pantalla en la olla para conseguir puntos"
+        resultado = {"tematica":tema,"descripcion":descripcion}
+        res_json = resultado.toJSON()
+        return HttpResponse(res_json,content_type ="text/json-comment-filtered")
+    
+    elif (numeroLevel == 1):
+        tema = "Fracciones"
+        descripcion = "Realiza una pizza con las fracciones que te diga la pantalla y consigue puntos"
+        resultado = {"tematica":tema,"descripcion":descripcion}
+        res_json = resultado.toJSON()
+        return HttpResponse(res_json,content_type ="text/json-comment-filtered")
+    
+    else:
+        tema = "Fracciones"
+        descripcion = "Realiza una pizza con las fracciones que te diga la pantalla y consigue puntos"
+        resultado = {"tematica":tema,"descripcion":descripcion}
+        res_json = resultado.toJSON()
+        return HttpResponse(res_json,content_type ="text/json-comment-filtered")
+
+
+def calificacion(response):
+    return HttpResponse("Calificacion funciona")
