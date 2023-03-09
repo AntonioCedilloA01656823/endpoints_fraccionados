f4024987de8950393246b4925867d74340b2b0d0
Antonio Rafael Cedillo Rodríguez <116749018+AntonioCedilloA01656823@users.noreply.github.com>
2023-03-08 19:49:48 -0600

    arreglos y los otros dos endpoints agregados
    nada mas faltarian 2 o 3 en el docs e implementados en codigo

--- fraccionados/endpoints/views.py
+++ fraccionados/endpoints/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from json import loads,dumps
+import random
 
 # Create your views here.
 class Nivel:
@@ -10,19 +11,51 @@ class Nivel:
         self.descripcion = descripcion
     def toJSON(self):
         return dumps(self, default=lambda o:o.__dict__, sort_keys=False, indent=4)
-    
+
+class Verificacion:
+    def __init__(self, valido, saludo):
+        self.valido = valido
+        self.saludo = saludo
+    def toJSON(self):
+        return dumps(self, default=lambda o:o.__dict__, sort_keys=False, indent=4)
+
+class Calificacion:
+    def __init__(self, numero):
+        self.numero = numero
+    def toJSON(self):
+        return dumps(self, default=lambda o:o.__dict__, sort_keys=False, indent=4)
 
 def index(request):
     return HttpResponse("La pagina indice funciona, Bienvenido a los endpoints")
 
-
+@csrf_exempt
 def login(request):
-    return HttpResponse("Login funciona")
+    print("Se realiza el login")
+    body = request.body.decode('UTF-8')
+    jsoncito = loads(body)
+    numLista = jsoncito['num_lista']
+    grupo = jsoncito['grupo']
+    grado = jsoncito['grado']
+
+    if (numLista in range(1,28)) and (grupo == "A") or (grupo == "B") and (grado in range(1,6)):
+        validation = True
+        greet = "Bienvenid@"
+        res = Verificacion(validation,greet)
+        res_json = res.toJSON()
+        return HttpResponse(res_json,content_type ="text/json-comment-filtered")
+    
+    else:
+        validation = False
+        greet = "Intenta de nuevo."
+        res = Verificacion(validation,greet)
+        res_json = res.toJSON()
+        return HttpResponse(res_json,content_type ="text/json-comment-filtered")
 
 
 
 @csrf_exempt
 def niveles(request):
+    print("Se realiza el output de niveles")
     body = request.body.decode('UTF-8')
     eljson = loads(body)
     numeroLevel = eljson['num_nivel']
@@ -55,6 +88,25 @@ def niveles(request):
         res_json = resultado.toJSON()
         return HttpResponse(res_json,content_type ="text/json-comment-filtered")
 
+@csrf_exempt
+def calificacion(request):
+    print("Realizando calificacion")
+    body = request.body.decode('UTF-8')
+    jsoncito = loads(body)
+    numLista = jsoncito['num_lista']
+    grupo = jsoncito['grupo']
+    grado = jsoncito['grado']
 
-def calificacion(response):
-    return HttpResponse("Calificacion funciona")
+    if (numLista in range(1,28)) and (grupo == "A") or (grupo == "B") and (grado in range(1,6)):
+        calif = random.randint(1,10)
+        res = Calificacion(calif)
+        res_json = res.toJSON()
+        return HttpResponse(res_json,content_type ="text/json-comment-filtered")
+    
+    else:
+        calif = 0
+        res = Calificacion(calif)
+        res_json = res.toJSON()
+        return HttpResponse(res_json,content_type ="text/json-comment-filtered")
+
+
